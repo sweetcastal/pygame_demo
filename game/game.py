e5866c732c87f829322c4fa8c6b016f7f5af30a0
Sakura <116530839+sweetcastal@users.noreply.github.com>
2023-08-09 21:58:15 +0800

    Add files via upload

---
+++ game/game.py
@@ -0,0 +1,16 @@
+# @Time:            2023/8/9 17:00
+# @User:            风前絮
+# @Site:            cloudfall.top
+# @File:            game
+# @Software:        PyCharm
+# @Author:          KazeMae
+# @Email:           xiaochunfeng.x@foxmail.com
+
+SCREEN_HEIGHT: int = 720
+SCREEN_WIDTH: int = 1280
+SCREEN_HALF_HEIGHT: int = 360
+SCREEN_HALF_WIDTH: int = 640
+
+FULL_WIDTH: int = 2800
+FULL_HEIGHT: int = 1200
+GAME_TITLE = "地道战"
